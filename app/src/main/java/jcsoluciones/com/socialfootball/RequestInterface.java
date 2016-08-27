1745c8b3a6669c010141ef6418d1a0330ddc715b
jorge <alexortizcortes@gmail.com>
2016-08-27 11:24:45 -0500

    reajuste servicios rest

--- app/src/main/java/jcsoluciones/com/socialfootball/RequestInterface.java
+++ app/src/main/java/jcsoluciones/com/socialfootball/RequestInterface.java
@@ -1,17 +1,20 @@
 package jcsoluciones.com.socialfootball;
 
+import java.util.List;
+
 import jcsoluciones.com.socialfootball.models.RequestTeamBody;
 import jcsoluciones.com.socialfootball.models.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface RequestInterface {
 
     @POST("team")
     Call<ResponseBody> registerTeam(@Body RequestTeamBody body);
 
-    @GET("searchteams")
-    Call<RequestTeamBody> searchTeam(@Body RequestTeamBody body);
+    @GET("searchteams/{name}/{city}")
+    Call<List<RequestTeamBody>> searchTeams(@Path("name") String name,@Path("city") String city);
 }
