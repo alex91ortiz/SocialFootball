ea16e44921e05cc1da3375cc464a3e0f110d2716
jorge <alexortizcortes@gmail.com>
2016-09-21 16:26:07 -0500

    ajustes para la creacion de eventos

--- app/src/main/java/jcsoluciones/com/socialfootball/EventsActivity.java
+++ app/src/main/java/jcsoluciones/com/socialfootball/EventsActivity.java
@@ -1,6 +1,6 @@
 package jcsoluciones.com.socialfootball;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
