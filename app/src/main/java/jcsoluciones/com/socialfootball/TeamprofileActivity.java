65fcd67615a93c49c1098a53790575a6af1e61d7
jorge <alexortizcortes@hotmail.com>
2016-09-24 02:08:38 -0500

    rediseño de plantillas

---
+++ app/src/main/java/jcsoluciones/com/socialfootball/TeamprofileActivity.java
@@ -0,0 +1,105 @@
+package jcsoluciones.com.socialfootball;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TeamprofileActivity extends AppCompatActivity {
+    private ViewPagerAdapter adapterViewpager;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teamprofile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        adapterViewpager = new ViewPagerAdapter(getSupportFragmentManager());
+        adapterViewpager.addFragment(new TeamMatchFragment(), "MATCHES");
+        adapterViewpager.addFragment(new TeamMatchFragment(), "INFORMATIONS");
+        adapterViewpager.addFragment(new TeamMatchFragment(), "PHOTOS");
+        //adapterViewpager.addFragment(new TournamentsFragment(), "FOUR");
+
+        viewPager.setAdapter(adapterViewpager);
+
+    }
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+            //return null;
+        }
+    }
+
+    public static class TeamMatchFragment extends Fragment {
+        public TeamMatchFragment() {
+            // Required empty public constructor
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_tournaments, container, false);
+        }
+    }
+}
